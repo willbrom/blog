dc60f2d14eeaca45ae1d6cecb6bc4b02d566c279
Ali Sajid <alisajid552@yahoo.com>
2020-11-19 18:26:37 -0800

    wrap app container inside BlogPorvider and return component

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
 import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/contex/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -15,4 +16,12 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
