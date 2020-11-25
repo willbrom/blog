9c652d51b89fd0e0d008f679bb996acc5b7719ac
Ali Sajid <alisajid552@yahoo.com>
2020-11-25 00:04:37 -0800

    create edit screen for editting blogs

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import BlogInput from "../components/BlogInput";
+import BlogContext from "../contex/BlogContext";
+
+const EditScreen = ({ navigation }) => {
+  const [title, setTitle] = useState(navigation.getParam("title"));
+  const [content, setContent] = useState(navigation.getParam("content"));
+  const { editBlogPost } = useContext(BlogContext);
+  const id = navigation.getParam("id");
+
+  return (
+    <View style={styles.containerStyle}>
+      <BlogInput title="New Title" value={title} setValue={setTitle} />
+      <BlogInput title="New Content" value={content} setValue={setContent} />
+      <Button
+        title="Save"
+        onPress={() => {
+          editBlogPost(id, title, content);
+          navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    marginTop: 12,
+  },
+});
+
+export default EditScreen;
