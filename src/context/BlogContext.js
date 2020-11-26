3c139497d71a794e587d9877296434da41bdc564
Ali Sajid <alisajid552@yahoo.com>
2020-11-25 23:44:47 -0800

    move from useState to useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,28 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const Reducer = (state, action) => {
+  // state === [{id, title, content}]
+  // action === { type: add || edit, payload: {id , title, content} }
+
+  switch (action.type) {
+    case "add":
+      return state.concat(action.payload);
+    case "edit":
+      return state.map((v) => (v.id == action.payload.id ? action.payload : v));
+    default:
+      break;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [state, dispatch] = useReducer(Reducer, []);
 
   const addBlogPost = (title, content) => {
-    setBlogPosts([
-      ...blogPosts,
-      { id: blogPosts.length.toString(), title, content },
-    ]);
+    dispatch({
+      type: "add",
+      payload: { id: state.length.toString(), title, content },
+    });
   };
 
   const editBlogPost = (id, title, content) => {
-    let temp = blogPosts.map((v) =>
-      v.id == id ? { ...v, title, content } : v
-    );
-    setBlogPosts(temp);
+    dispatch({
+      type: "edit",
+      payload: { id, title, content },
+    });
   };
 
   return (
-    <BlogContext.Provider
-      value={{ data: blogPosts, addBlogPost, editBlogPost }}
-    >
+    <BlogContext.Provider value={{ data: state, addBlogPost, editBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
