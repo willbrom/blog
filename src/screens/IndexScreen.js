6eadc39950bf5f5068085ffaa8402ba17fc7f28e
Ali Sajid <alisajid552@yahoo.com>
2020-12-01 21:33:37 -0800

    use getBlogPosts action from context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,7 +10,9 @@ import { Context as BlogContext } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(BlogContext);
+  const { state, deleteBlogPost, getBlogPosts } = useContext(BlogContext);
+
+  getBlogPosts();
 
   return (
     <View style={styles.containerStyle}>
